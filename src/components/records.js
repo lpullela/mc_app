a97bd625b457f36831e2a36b214d9f7581ad4f5b
lpullela <91499229+lpullela@users.noreply.github.com>
2022-09-02 12:49:47 -0700

    initial commits

---
+++ src/components/records.js
@@ -0,0 +1,16 @@
+import React from 'react'; 
+
+function Records() { 
+    return(
+        <section> 
+            <div class="container-fluid">
+                <h1 class="mt-3">What we want here:</h1>
+                <p>Patient Login</p>
+                <p>Concern and Emergency Level</p>
+                <p>Contact Information</p>
+            </div>
+        </section>
+    )
+}
+
+export default Records;
